234cbbe2de772c1a26a49a5f2fa58b4a4c4ac325
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-08-02 20:13:48 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.plugin.statistics.ReportStatisticsToElasticSearch.url
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val corda_gradle_plugins_version: String by project
+val corda_release_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
+}
+
+group = "com.rewera"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+
+    maven { url = uri("https://software.r3.com/artifactory/corda") }
+    maven { url = uri("https://software.r3.com/artifactory/corda-dependencies") }
+    maven { url = uri("https://software.r3.com/artifactory/corda-dev") }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("net.corda:corda-core:$corda_release_version")
+}
