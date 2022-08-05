7619240c3a7b0ae2d25ffb954074bed745fd1225
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-08-05 12:53:36 +0100

    Add dummy endpoints to start with

--- src/main/kotlin/com/rewera/plugins/Routing.kt
+++ src/main/kotlin/com/rewera/plugins/Routing.kt
@@ -1,5 +1,7 @@
 package com.rewera.plugins
 
+import com.rewera.connectors.CordaNodeConnector
+import com.rewera.connectors.CornectorRpcOps
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -12,5 +14,44 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
+
+        flowManagerRoutes()
+        flowStarterRoutes()
+        vaultQueryRoutes()
+    }
+}
+
+fun Route.flowManagerRoutes() {
+    route("/flowmanagerrpcops") {
+
+        post("/killflow/{flowid}") {}
+
+        get("/listactive") {}
+
+        get("/getprotocolversion") {}
+    }
+}
+
+fun Route.flowStarterRoutes() {
+    route("/flowstarter") {
+
+        get("/registeredflows") {}
+
+        post("/startflow") {}
+
+        get("/flowoutcomeforclientid/{clientid}") {}
+
+        get("/flowoutcome/{flowid}") {}
+
+        get("/getprotocolversion") {}
+    }
+}
+
+fun Route.vaultQueryRoutes() {
+    route("/vaultqueryrpc") {
+
+        post("/queryvault") {}
+
+        get("/getprotocolversion") {}
     }
 }
